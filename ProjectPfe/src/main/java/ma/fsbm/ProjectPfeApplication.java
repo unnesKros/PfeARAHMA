fa9dec1e68aaa38c62d518eea3092e1eb841ce93
alaa <alaa@alaa-HP-ProBook-430-G3>
2022-06-01 23:11:02 +0100

    dddddddddd

--- ProjectPfe/src/main/java/ma/fsbm/ProjectPfeApplication.java
+++ ProjectPfe/src/main/java/ma/fsbm/ProjectPfeApplication.java
@@ -33,6 +33,7 @@ public class ProjectPfeApplication {
 			/*Client client=new Client(null, "sabile","hafa", "nom", "prenom", null, null, null, null, null, null, null) ;
 		    clientRepository.save(client);*/
 			/*new upate*/
+			/*youness ras l9lwa*/
 		    
 		     
 		};
